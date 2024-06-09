060803d8175641ce6ff5d87dece4ec9afc8d444f
galahad-arpit03 <arpitgupta00090@gmail.com>
2024-06-10 03:38:30 +0530

    2nd commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Lets make new projects.
         </p>
-        <a href="mailto:arpitgupta9907@gmail.com">  
+        <a href="mailto:arpitgupta9907@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
@@ -42,7 +42,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <a href={info.link}>
+              <a href={info.link} target="_blank">
                 <img src={info.img} alt="icons" width={20} height={20} />
               </a>
             </div>
--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,5 @@
+import { link } from "fs";
+
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
@@ -8,12 +10,12 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "I keen on building beautiful and functional websites",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/developer.jpg",
     spareImg: "",
   },
   {
@@ -49,7 +51,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building an online banking  platform",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -72,34 +74,66 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    title: "Portfolio Website - Next.Js",
+    des: "A modern portfolio website built with Next.js, Tailwind CSS and Typescript",
+    img: "/Portfolio.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
     link: "/ui.earth.com",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    title: "E-commerce Website with Next.Js",
+    des: "A modern e-commerce website built with Next.js, Tailwind CSS , Typescript and Clerk",
+    img: "/ecommerce.jpeg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
     link: "/ui.yoom.com",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    title: "Attendance Management System with Next.Js",
+    des: "A modern attendance management system built with Next.js, Tailwind CSS and MongoDB",
+    img: "/AMS.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
     link: "/ui.aiimg.com",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    title: "Alumni Management System with PHP and MySQL",
+    des: "A modern alumni management system built with PHP, MySQL and Bootstrap",
+    img: "/Alumni.jpeg",
+    iconLists: [],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 5,
+    title: "Learning Management System with React",
+    des: "Learning Management System built with React, Tailwind CSS and Firebase",
+    img: "/LMS.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 6,
+    title: "Facebook Clone ",
+    des: "Facebook Clone built with React",
+    img: "/face.jpeg",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 7,
+    title: "Instagram Clone",
+    des: "Instagram Clone Frontend build with HTML, CSS and JS",
+    img: "/instagram.jpeg",
+    iconLists: [],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 8,
+    title: "Spotify Clone",
+    des: "a Spotify Clone built with React, Tailwind CSS and Spotify API",
+    img: "/spotify.jpeg",
+    iconLists: ["/re.svg", "/tail.svg"],
     link: "/ui.apple.com",
   },
 ];
@@ -107,33 +141,33 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I achieved the 3rd highest rank in my 5th semester, reflecting my dedication and consistent hard work. This accomplishment highlights my commitment to academic excellence and my ability to excel in a competitive environment.",
+    name: "",
+    title: "",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I proudly represented our college in the state selection tournament for basketball, showcasing my athletic skills, teamwork, and determination. This experience demonstrated my ability to excel in sports and my commitment to representing my college at a high level.",
+    name: "",
+    title: "",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I coordinated various events at a national level fest, demonstrating my organizational abilities, leadership skills, and commitment to extracurricular activities. This experience allowed me to effectively manage teams, handle logistics, and ensure the successful execution of multiple events.",
+    name: "",
+    title: "",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I am a Google Certified UI and UX Designer, having completed comprehensive training that underscores my proficiency in creating user-friendly and visually appealing interfaces. This certification demonstrates my expertise in user experience principles, design thinking, and my commitment to maintaining high standards in digital design.",
+    name: "",
+    title: "",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I have completed numerous certifications in my key skills, training under various instructors from renowned colleges and companies, including IBM. These certifications highlight my dedication to continuous learning and proficiency in diverse areas, ensuring I stay updated with industry standards and best practices.",
+    name: "",
+    title: "",
   },
 ];
 
@@ -205,13 +239,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/galahad-arpit03",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://twitter.com/_galahad03",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/galahad-arpit/",
   },
 ];
